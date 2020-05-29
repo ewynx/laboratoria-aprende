96852980f48afcbb7998b55e32c4f4de75af08d4
Fuentes <E.Fuentes.Bongenaar@protonmail.com>
2020-05-29 16:14:41 -0600

    Simple Cordova project that opens the Laboratoria Aprende page.

---
+++ www/js/index.js
@@ -0,0 +1,14 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        window.location= "https://aprende.laboratoria.la";
+    }
+};
+
+app.initialize();
